668bd2c9a5e1e6329b2d18ca0f0e9e0f3bba2d94
Andrae Lewis <andrae.lewis@gmail.com>
2020-06-06 14:00:05 -0400

    Update Stella.py

--- Python/Stella.py
+++ Python/Stella.py
@@ -5,7 +5,7 @@ split_string = string_given.split(" ")
 new_list = []
 
 for words in split_string:
-    if words[0].lower() in  ['a','e','i','o','u']:
+    if words [0].lower() in  ['a','e','i','o','u']:
         new_list.append(words)
         
 print(" ".join(new_list))
